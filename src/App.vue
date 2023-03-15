429af750c48cba27e46091bb9de648583113a232
Alessandro-Villani <alessvill@libero.it>
2023-03-15 16:06:34 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'Boolfolio',
+}
+</script>
+
+<template></template>
+
+<style lang="scss">
+@use './assets/sass/style.scss';
+</style>
---
+++ src/main.js
@@ -0,0 +1,8 @@
+import { createApp } from 'vue';
+import App from './App.vue';
+// BOOTSTRAP
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const app = createApp(App);
+
+app.mount('#app');
