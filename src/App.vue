f06d77013c9befa3b9298e072ddebdff7a64c2fe
Alessandro-Villani <alessvill@libero.it>
2023-03-15 16:27:54 +0100

    added fetch projects

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,30 @@
 <script>
 import axios from 'axios';
 import AppHeader from './components/AppHeader.vue';
+const apiBaseUrl = 'http://127.0.0.1:8000/api/';
 
 export default {
   name: "Boolfolio",
-  components: { AppHeader }
+  components: { AppHeader },
+  data() {
+    return {
+      projects: [],
+    }
+  },
+  methods: {
+    fetchProjects() {
+      axios.get(apiBaseUrl + 'projects').then(res => {
+        this.projects = res.data;
+      }).catch(e => {
+        console.error(e);
+      }).then(() => {
+
+      })
+    }
+  },
+  created() {
+    this.fetchProjects();
+  }
 }
 </script>
 
